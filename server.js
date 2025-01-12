85f6e074321a5b68acc8906741980fdf1c3aa037
Nicole Riera <150852807+NicoRi5@users.noreply.github.com>
2025-01-12 12:50:08 -0500

    fix errors and add router

--- server.js
+++ server.js
@@ -1,24 +1,27 @@
 const express = require("express");
 const { v4: uuidv4 } = require("uuid");
 const app = express();
+const router = express.Router();
 const PORT = 3000;
+app.use(express.json());
+app.use("/", router);
 
-app.get("/", (req, res) => {
+router.get("/", (req, res) => {
   res.send("Hello employees!");
 });
 
 const employees = require("./employees");
 
-app.get("/employees", (req, res) => {
+router.get("/employees", (req, res) => {
   res.json(employees);
 });
 
-app.get("/employees/random", (req, res) => {
+router.get("/employees/random", (req, res) => {
   const i = Math.floor(Math.random() * employees.length);
   res.json(employees[i]);
 });
 
-app.get("/employees/:id", (req, res) => {
+router.get("/employees/:id", (req, res) => {
   const { id } = req.params;
   const employee = employees.find((e) => e.id === +id);
   if (employee) {
@@ -29,19 +32,19 @@ app.get("/employees/:id", (req, res) => {
 });
 
 // POST /employees will add new employee with provided name (if correctly provided)
-app.post("/employees", (req, res, next) => {
+router.post("/employees", (req, res, next) => {
   const { name } = req.body;
   if (!name?.trim()) {
     return res.status(400).send("No correctly provided name!");
   }
 
-  employees.push({ id: uuidvd(), name: name.trim });
+  employees.push({ id: uuidv4(), name: name.trim() });
   res.send(employees);
 });
 
 // 404 and error-handling middleware:
 app.use((req, res, next) => {
-  next({ status: 404, message: "Not found." });
+  res.status(404).send("Not Found.");
 });
 
 // error-handling:
@@ -52,5 +55,5 @@ app.use((err, req, res, next) => {
 });
 
 app.listen(PORT, () => {
-  `Listening on port ${PORT}...`;
+  console.log(`Listening on port ${PORT}...`);
 });
